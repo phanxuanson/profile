2b31c0b6e13e6a406f7333591982dc085e02ac0b
unknown <phanxuanson92@gmail.com>
2025-09-19 15:30:13 +0700

    update vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: "https://astro-resume-theme.netlify.app",
-  integrations: [tailwind(), icon(), mdx(), sitemap()]
+  integrations: [tailwind(), icon(), mdx(), sitemap()],
+  adapter: vercel()
 });
